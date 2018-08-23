9dd71df4d3889b09a33551016c9485be84d8b6cc
BartoszLenart <bartek9513@interia.pl>
2018-08-23 13:30:14 +0200

    initial

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+'use strict';
+
+(function(){
+    
+    function Cal(cont, result) {
+        this.input = document.querySelectorAll('.' + cont + ' input[type=\'button\']');
+        this.resultDisplay = document.querySelector('#' + result);
+        this.assign();
+    }
+    
+    Cal.prototype.assign = function() {
+        for (let i in this.input) {
+            this.input[i].addEventListener('click', this.calculate.bind(this));
+        }
+    }
+    
+    var arr = [];
+    Cal.prototype.calculate = function(e) {
+        console.log('sth works');
+        
+        if (e.target.value == "=") {
+            var res1 = eval(this.resultDisplay.value);
+            arr = [res1];
+            this.resultDisplay.value = arr[0];
+            return;
+        }
+        
+        if (e.target.value == "C") {
+            this.resultDisplay.value = "";
+            arr = [];
+            return;
+        }
+        
+        arr.push(e.target.value);
+        if(arr[arr.length-1] == "." && arr[arr.length-2] == ".") {
+            arr.pop();
+        }
+
+        console.log(arr);
+        this.resultDisplay.value = "";
+        for (let j in arr) {
+            this.resultDisplay.value += arr[j];
+        }
+    }
+    
+    new Cal('calc', 'res');
+})()
+
+
